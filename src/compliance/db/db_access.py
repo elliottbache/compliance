7f7dc9bb9fa02ffd15b53c058c01820d574b4301
Correct Name <elliottbache@gmail.com>
2026-04-28 19:58:40 +0200

    fix: enhance database session management and add naming conventions for metadata

--- src/compliance/db/db_access.py
+++ src/compliance/db/db_access.py
@@ -1,7 +1,9 @@
+from collections.abc import Generator
 from os import getenv
 
 from dotenv import load_dotenv
 from sqlalchemy import Engine, MetaData, Table, create_engine
+from sqlalchemy.orm import Session
 
 load_dotenv()
 DB_NAME = getenv("POSTGRES_DB")
@@ -11,6 +13,22 @@ HOST = getenv("POSTGRES_HOST")
 if DB_NAME is None or USER is None or PASSWORD is None or HOST is None:
     raise ValueError(".env value is not being read correctly.")
 DB_URL = "postgresql+psycopg2://" + USER + ":" + PASSWORD + "@" + HOST + "/" + DB_NAME
+
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+
+engine = create_engine(DB_URL)
+metadata = MetaData(naming_convention=convention)
+
+
+def get_db() -> Generator:
+    with Session(engine) as session:
+        yield session
 
 
 def get_engine_metadata() -> tuple[Engine, MetaData]:
